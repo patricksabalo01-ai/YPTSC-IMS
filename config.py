b00e0009cc58c6c29ba72bbc81232a149940024d
IT-PATRICK <patrick.sabalo.01@gmail.com>
2026-08-05 16:41:53 +0800

    Refactor Config class for cleaner environment variable access

--- config.py
+++ config.py
@@ -1,37 +1,14 @@
 import os
-
 from dotenv import load_dotenv
-
 load_dotenv()
-
-
 class Config:
-
-    SECRET_KEY = os.environ.get(
-        "YPTSC_SECRET_KEY",
-        "YPTSC-IMS-DEV-KEY"
-    )
-
-    # ==================================
+    SECRET_KEY = os.environ.get("YPTSC_SECRET_KEY","YPTSC-IMS-DEV-KEY")
     # POSTGRESQL
-    # ==================================
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    )
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # ==================================
     # FLASK
-    # ==================================
-
     TEMPLATES_AUTO_RELOAD = True
-
     SESSION_PERMANENT = False
-
     SESSION_COOKIE_HTTPONLY = True
-
     SESSION_COOKIE_SAMESITE = "Lax"
-
     SESSION_COOKIE_SECURE = False
