f73e9f3e4f1dc3931ff9a9e39e5187062e7d2302
IT-PATRICK <patrick.sabalo.01@gmail.com>
2026-08-05 16:42:56 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,47 +5,9 @@ FLASK APPLICATION STARTUP
 Enterprise v1
 ==================================================
 """
-
-
 from app import create_app
-
-
-
-
-
-# ==================================
-# CREATE APPLICATION INSTANCE
-# ==================================
-
-
+#CREATE APPLICATION INSTANCE
 app = create_app()
-
-
-
-
-
-
-
-# ==================================
-# APPLICATION RUNNER
-# ==================================
-
-
+#APPLICATION RUNNER
 if __name__ == "__main__":
-
-
-
-    app.run(
-
-
-        host="0.0.0.0",
-
-
-        port=5000,
-
-
-        debug=True
-
-
-
-    )
+    app.run(host="0.0.0.0",port=5000,debug=True)
